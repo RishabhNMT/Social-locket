d1d9129bc68f4da223c92c5e1a57d61239855b2b
RishabhNMT <rishabh@nmttechnologies.com>
2022-12-27 18:13:23 +0530

    27 dec changes

--- src/ComingSoon/ComingSoon.js
+++ src/ComingSoon/ComingSoon.js
@@ -2,28 +2,70 @@ import React, { useState } from 'react'
 import '../ComingSoon/Coming.css'
 import logo from '../ComingSoon/Comindimage/logo.png'
 import { RestService } from '../Services/RestService';
-
+import validator from 'validator';
 
 const ComingSoon = () => {
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState(false);
-
+    const [emailError, setEmailError] = useState("");
     const setval = (e) => {
         setEmail(e.target.value)
     }
-    const subscribe_click = async (e) => {
-        e.preventDefault()
-        console.log("email", email)
-        let body = { "email": email }
-        let res = await RestService.comingsoon(body);
-        console.log("data", res)
-        if (res.status === 200) {
-            setEmail("");
-            setMessage(true)
+    const validateEmail = (e) => {
+        setEmail(e.target.value)
+        console.log("data", e.target.value)
+
+        var email = e.target.value
+        if (validator.isEmail(email) || validator.isEmpty(email)) {
+            if (validator.isEmpty(email)) {
+                setEmailError('')
+            }
+            else {
+                setEmailError('')
+            }
+
+        } else {
+            setEmailError('Enter valid Email!')
+        }
+    }
+    const checkEmailValidation = (e) => {
+        var validated = false;
+
+        var emailid = email
+        if (validator.isEmpty(emailid)) {
+            setEmailError('Enter Email')
+            validated = false;
         }
         else {
+            if (validator.isEmail(emailid)) {
+                setEmailError('')
+                validated = true;
+            }
+            else {
+                setEmailError('Enter Valid Email')
+            }
         }
-        console.log("message", message)
+        return validated;
+    }
+    const subscribe_click = async (e) => {
+        e.preventDefault()
+        setEmailError('');
+        console.log(email)
+        const isEmailValid = checkEmailValidation();
+        if (email !== '' && isEmailValid) {
+            console.log("email", email)
+            let body = { "email": email }
+            let res = await RestService.comingsoon(body);
+            console.log("data", res)
+            if (res.status === 200) {
+                setEmail("");
+                setMessage(true)
+            }
+        }
+        else {
+
+        }
+
     }
     return (
         <>
@@ -57,9 +99,10 @@ const ComingSoon = () => {
                             <p>We're coming soon! We're working on our brand new website and will be launching soon. Do not miss it,
                                 subscribe below to keep updated.</p>
                             <div className="subscribe">
-                                <input type="email" id="email" name="email" value={email} onChange={setval} placeholder="Enter your email" />
+                                <input type="email" id="email" name="email" value={email}
+                                    onChange={validateEmail} placeholder="Enter your email" />
                                 <button type="button" id="subcribe" onClick={subscribe_click}>Subscribe</button>
-                            </div>
+                            </div> <span style={{ color: 'white' }}>{emailError}</span>
                         </div>
                     }
                 </div>
